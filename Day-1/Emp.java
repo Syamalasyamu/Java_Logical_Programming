2a7b93363c6118c2883841372fb83786476aeb4c
Syamalasyamu <syamusyamala55@gmail.com>
2024-11-07 16:44:36 +0530

    Add files via upload

---
+++ Day-1/Emp.java
@@ -0,0 +1,10 @@
+class Emp{
+public static void main(String args[]){
+  int Number=101;
+  String Name="syamu";
+  float Slary=544.45F;
+  System.out.println("The Given Employee number:"+Number);
+  System.out.println(" The Given Employee Name:"+Name);
+  System.out.println("The Given Employee slary:"+Slary);
+  }
+}
---
+++ Day-1/Second.java
@@ -0,0 +1,13 @@
+// sample java program
+/* 
+    sample
+    java
+    program 
+ */
+import java.lang.*;
+
+class Second{
+public static void main(String args[]){
+System.out.print("welcome to java program");
+} 
+}
